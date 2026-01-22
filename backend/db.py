d33c9512d99568f8f494aa62c237bb5b90116b8c
Tamilmani N <ntamilmanin@gmail.com>
2026-01-22 07:22:19 +0000

    Day 11: Docker and Docker Compose setup for Flask and MySQL

--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,10 @@
+import os
 import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host="aws-rds-endpoint.amazonaws.com",  # MySQL LAN IP
-        user="ticket_user",
-        password="StrongPass",
-        database="ticket_rising"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
